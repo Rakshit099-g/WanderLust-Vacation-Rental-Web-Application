15391a9553831e7fb6a8b5fc993768a142aa7044
rakshit <hg841731@gmail.com>
2026-07-11 10:33:11 +0530

    fixation

--- utils/sendOtp.js
+++ utils/sendOtp.js
@@ -45,15 +45,15 @@ It will expire in 5 minutes.
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-    tls: {
-        rejectUnauthorized: false,
-    },
+//     tls: {
+//         rejectUnauthorized: false,
+//     },
 });
 
 const generateAndSendOtp = async (email)=>{
